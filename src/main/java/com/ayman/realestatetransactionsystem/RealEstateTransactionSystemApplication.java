12bbcace1b246301cb5f0ecdc2111f5b30fab71e
aymanalharb <ayman.f.alharbi@gmail.com>
2025-07-21 13:49:56 +0300

    First commit

---
+++ src/main/java/com/ayman/realestatetransactionsystem/RealEstateTransactionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ayman.realestatetransactionsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateTransactionSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealEstateTransactionSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ayman/realestatetransactionsystem/RealEstateTransactionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayman.realestatetransactionsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateTransactionSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
