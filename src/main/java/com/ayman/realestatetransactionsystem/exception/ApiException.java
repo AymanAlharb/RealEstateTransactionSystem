ebb3d3633176d06ab8325350efe787654b0f5ff7
aymanalharb <ayman.f.alharbi@gmail.com>
2025-07-21 13:54:28 +0300

    Exceptions package

---
+++ src/main/java/com/ayman/realestatetransactionsystem/exception/ApiException.java
@@ -0,0 +1,7 @@
+package com.ayman.realestatetransactionsystem.exception;
+
+public class ApiException extends RuntimeException {
+    public ApiException(String message) {
+        super(message);
+    }
+}
